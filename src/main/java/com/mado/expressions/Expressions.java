47f002cf222b0a71aa53a2b09f1122f02ed73f8a
MagyoDev <alandelimasilva51@gmail.com>
2024-03-06 00:12:12 -0300

    Expressions

---
+++ src/main/java/com/mado/expressions/Expressions.java
@@ -0,0 +1,23 @@
+package com.mado.expressions;
+
+public class Expressions {
+
+    public static void main(String[] args) {
+        // Expressions = operands & operators
+        // Operands = values, variables, numbers, quantity
+        // Operators = + - * / %
+        
+        int friends = 10;
+        
+        friends = friends + 2; 
+        //friends = friends - 2;
+        //friends = friends * 2;
+        //friends = friends / 2;
+        //friends = friends % 3;
+        //friends++;
+        //friends--;
+        //friends = (double) friends / 3;
+        
+        System.out.println(friends);
+    }
+}
